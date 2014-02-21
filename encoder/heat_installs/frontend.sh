77b42e53c72698dde18ae4d2b0e81f465d01580d
Mike Metral <metral@gmail.com>
2014-02-21 20:24:10 +0000

    Update git clone to proper dir

--- encoder/heat_installs/frontend.sh
+++ encoder/heat_installs/frontend.sh
@@ -4,7 +4,7 @@ WEBAPP_IP=$1
 
 sudo apt-get update
 sudo apt-get install git -y
-git clone git@github.com:metral/touchstone.git ~/
+git clone git@github.com:metral/touchstone.git ~/touchstone
 
 cd ~/touchstone/encoder/frontend
 
