7d890df41ad2d3f824c38a9c6bef048bf66ac846
Mike Metral <metral@gmail.com>
2014-02-26 20:48:24 +0000

    Add basic upload ability

--- encoder/webapp/django/converter/views.py
+++ encoder/webapp/django/converter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 #-------------------------------------------------------------------------------
 def converter_index(request):
     page = "converter"
@@ -15,4 +16,15 @@ def converter_index(request):
         rendered_response = render_to_response(\
                 template, data, context_instance = context_instance)
         return rendered_response
+    if request.method == "POST":
+        for key, file in request.FILES.items():
+            path = file.name
+            dest = open(path, 'w')
+            if file.multiple_chunks:
+                for c in file.chunks():
+                    dest.write(c)
+            else:
+                dest.write(file.read())
+            dest.close()
+        return HttpResponseRedirect('/')
 #-------------------------------------------------------------------------------
