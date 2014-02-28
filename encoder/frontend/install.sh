910fc7d1bf40643be995a5349efca01afdab8380
Mike Metral <metral@gmail.com>
2014-02-28 23:13:07 +0000

    Enable https & forward http to https

--- encoder/frontend/install.sh
+++ encoder/frontend/install.sh
@@ -8,7 +8,9 @@ sudo apt-get install nginx htop -y
 
 # soft link the upstart job files
 PWD=`pwd`
+mkdir /etc/nginx/ssl
 sudo cp -f $PWD/encoder /etc/nginx/sites-available/
+sudo cp -f $PWD/server* /etc/nginx/ssl/
 sudo ln -s -f /etc/nginx/sites-available/encoder /etc/nginx/sites-enabled/encoder
 sudo rm /etc/nginx/sites-enabled/default
 
