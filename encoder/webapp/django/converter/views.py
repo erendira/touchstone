2b12e1b2108edab00aba750976ed360a7a5ac818
Mike Metral <metral@gmail.com>
2014-02-28 22:41:49 +0000

    Add prefix to cloudfiles

--- encoder/webapp/django/converter/views.py
+++ encoder/webapp/django/converter/views.py
@@ -14,6 +14,8 @@ import pyrax
 import pyrax.exceptions as exc
 import pyrax.utils as utils
 import sys
+import random
+import string
 #-------------------------------------------------------------------------------
 pyrax.set_setting("identity_type", "rackspace")
 creds_file = os.path.expanduser("~/pyrax_rc")
@@ -21,40 +23,45 @@ region = "ORD"
 pyrax.set_credential_file(creds_file, region)
 cf = pyrax.cloudfiles
 container = "submitted"
-cont = cf.create_container(container)
-cont.make_public(ttl=1200)
 meta = {"x-account-meta-temp-url-key": "rackspace_rocks"}
 cf.set_account_metadata(meta)
+sc = pyrax.identity.services
 #-------------------------------------------------------------------------------
 def get_form_post_sig(cf_path, redirect_url, max_file_size, 
         max_file_count, expires):
 
     key = cf.get_account_metadata()['x-account-meta-temp-url-key']
-
     hmac_body = '%s\n%s\n%s\n%s\n%s' % (cf_path, redirect_url,
         max_file_size, max_file_count, expires)
-
     sig = hmac.new(key, hmac_body, sha1).hexdigest()
+
     return sig
 #-------------------------------------------------------------------------------
+def get_random_str(length):
+    return ''.join([random.choice(string.ascii_letters + string.digits) for n
+        in xrange(length)])
+#-------------------------------------------------------------------------------
 def converter_index(request):
-    sc = pyrax.identity.services
+    cont = cf.create_container(container)
+    cont.make_public(ttl=1200)
     cf_public_endpoint = sc['object_store']['endpoints'][region]['public_url']
     cf_url = cf_public_endpoint + "/" + container
 
     parsed_uri = urlparse(cf_public_endpoint)
     path = '{uri.path}/'.format(uri=parsed_uri)
 
+
+    prefix = str(int(time())) + "_" + get_random_str(8) + "_"
     cf_path = path + container
-    redirect_url = 'http://23.253.219.5'
+    redirect_url = "https://" + request.META['SERVER_NAME'] + "/status/submitted"
     max_file_size = 104857600
     max_file_count = 10
-    expires = int(time() + 600)
-    sig = get_form_post_sig(cf_path, redirect_url, max_file_size, max_file_count,
+    expires = int(time() + 1200)
+    sig = get_form_post_sig(cf_path + "/" + prefix, redirect_url, max_file_size, max_file_count,
             expires)
 
     data = {
-            'cf_url': cf_url,
+            'cf_url': cf_url + "/" + prefix,
             'redirect_url': redirect_url,
             'max_file_size': max_file_size,
             'max_file_count': max_file_count,
