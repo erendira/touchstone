6206f498a84a75fa6cd8e6610933f951421213b1
Mike Metral <metral@gmail.com>
2014-03-18 20:13:33 +0000

    Fix error of missing use_snet param

--- encoder/server_userdata/data_worker.sh
+++ encoder/server_userdata/data_worker.sh
@@ -4,7 +4,8 @@ RAX_USERNAME=$1
 RAX_APIKEY=$2
 DATA_MASTER_IP=$3
 MYSQL_PASS=$4
-BRANCH=$5
+USE_SNET=$5
+BRANCH=$6
 
 sudo apt-get update
 sudo apt-get install git -y
@@ -16,4 +17,4 @@ if [ "$BRANCH" != "master" ]; then
     git checkout -b $BRANCH origin/$BRANCH
 fi
 
-./install.sh $RAX_USERNAME $RAX_APIKEY $DATA_MASTER_IP $MYSQL_PASS
+./install.sh $RAX_USERNAME $RAX_APIKEY $DATA_MASTER_IP $MYSQL_PASS $USE_SNET
