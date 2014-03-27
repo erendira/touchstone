0530e4e520e5c06bcf1e867a9d1bbbdfda8c5927
Mike Metral <metral@gmail.com>
2014-03-27 22:33:08 +0000

    Change default ubuntu hadoop image

--- bigdata/savanna_setup.sh
+++ bigdata/savanna_setup.sh
@@ -34,8 +34,8 @@ TOKEN=`keystone token-get | grep id | grep -v user_id | awk '{print $4}'`
 export AUTH_TOKEN=`echo $TOKEN | cut -d ' ' -f1`
 export TENANT_ID=`echo $TOKEN | cut -d ' ' -f2`
 
-wget http://abed605ffd85ad8177a1-c04b8ff82efc0caa202f092894159bbe.r10.cf1.rackcdn.com/sahara-icehouse-vanilla-1.2.1-ubuntu-13.10.qcow2
-glance image-create --name=sahara-0.3-icehouse-vanilla-hadoop-1.2.1-ubuntu-13.10 --disk-format=qcow2 --container-format=bare < ./sahara-icehouse-vanilla-1.2.1-ubuntu-13.10.qcow2
+wget http://abed605ffd85ad8177a1-c04b8ff82efc0caa202f092894159bbe.r10.cf1.rackcdn.com/savanna-0.3-vanilla-1.2.1-ubuntu-13.04.qcow2
+glance image-create --name=savanna-0.3-vanilla-1.2.1-ubuntu-13.04.qcow2 --disk-format=qcow2 --container-format=bare < ./savanna-0.3-vanilla-1.2.1-ubuntu-13.04.qcow2
 export IMAGE_ID=`glance image-list | grep sahara | awk '{print $2}'`
 export IMAGE_USER="ubuntu"
 
