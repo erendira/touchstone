f9d64c7fb3ba3eab4da7ebd0f184e2a40d631a4a
Mike Metral <metral@gmail.com>
2014-03-27 03:53:36 +0000

    Add new hadoop port

--- bigdata/savanna_setup.sh
+++ bigdata/savanna_setup.sh
@@ -106,6 +106,7 @@ http $SAVANNA_URL/cluster-templates X-Auth-Token:$AUTH_TOKEN < cluster_template_
 
 # Setup security groups
 
+nova secgroup-add-rule default tcp 50010 50010 0.0.0.0/0
 nova secgroup-add-rule default tcp 50030 50030 0.0.0.0/0
 nova secgroup-add-rule default tcp 50060 50060 0.0.0.0/0
 nova secgroup-add-rule default tcp 50070 50070 0.0.0.0/0
