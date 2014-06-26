84efbb2dc4a501d834427202445da8118861b58b
Mike Metral <metral@gmail.com>
2014-06-26 12:27:13 -0700

    Remove request metadata usage

--- logging/webapp/django/home/views.py
+++ logging/webapp/django/home/views.py
@@ -3,15 +3,14 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from django.template import RequestContext
 from helloworld_proj import env_settings
+import logging
+#-------------------------------------------------------------------------------
+logger = logging.getLogger(__name__)
 #-------------------------------------------------------------------------------
 def home_index(request):
     page = "home"
-    referrer_http_host = request.META['HTTP_HOST']
-    referrer_remote_addr = request.META['REMOTE_ADDR']
     data = {
             'page': page,
-            'referrer_http_host': referrer_http_host,
-            'referrer_remote_addr': referrer_remote_addr,
             'hostname': env_settings.HOSTNAME,
             }
     template = "home/index.html"
