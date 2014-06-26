083e32117d61a709c934a211d2a1a951c01c324b
Mike Metral <metral@gmail.com>
2014-06-25 17:31:11 -0700

    Dont overclone repo for syslog client & fix comments

--- logging/rsyslog/client/install.sh
+++ logging/rsyslog/client/install.sh
@@ -10,7 +10,6 @@ RSYSLOG_SERVER_IP=$1
 
 sed -e "s#{RSYSLOG_SERVER_IP}#$RSYSLOG_SERVER_IP#g" \
     nginx_template.conf | \
-    tee /tmp/nging
-    #tee /etc/rsyslog.d/nginx.conf > /dev/null
+    tee /etc/rsyslog.d/nginx.conf > /dev/null
 
-#service rsyslog restart
+service rsyslog restart
--- logging/server_userdata/rsyslog_client.sh
+++ logging/server_userdata/rsyslog_client.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install git -y
 
 CLONE_PATH=~/touchstone
-git clone https://github.com/metral/touchstone.git $CLONE_PATH
+#git clone https://github.com/metral/touchstone.git $CLONE_PATH
 pushd $CLONE_PATH
 
 if [ "$BRANCH" != "master" ]; then
