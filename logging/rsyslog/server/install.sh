cb0851646b4852f46fa8701257f90d413b6bb9a6
Mike Metral <metral@gmail.com>
2014-06-30 12:53:27 -0700

    Fix syslog directory ownership to syslog

--- logging/rsyslog/server/install.sh
+++ logging/rsyslog/server/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo mkdir -p /var/log/logging_stack
+RSYSLOG_PATH="/var/log/logging_stack"
+sudo mkdir -p $RSYSLOG_PATH
+sudo chown -R syslog:syslog $RSYSLOG_PATH
+
 sudo cp -f ./50-default.conf /etc/rsyslog.d/
 sudo cp -f ./rsyslog.conf /etc/
 sudo service rsyslog restart
